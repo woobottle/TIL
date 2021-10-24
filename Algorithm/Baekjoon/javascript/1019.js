33ce5987a456e3291ba5ab17df089d95e3ff4d8b
woobottle <dkrnfls@gmail.com>
2021-10-25 08:36:30 +0900

    add algorithm

--- Algorithm/Baekjoon/javascript/1019.js
+++ Algorithm/Baekjoon/javascript/1019.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split(' ');
-const first = +input[0];
-const first = 19;
+// const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+// const first = +input[0];
+const first = 543212345;
 const result = Array.from({length: 10}, (i) => 0)
 
 const calc = (start, end, ten) => {
-  count = ~~(start / 10) - ~~(end / 10) + 1
+  count = ~~(end / 10) - ~~(start / 10) + 1
   result.forEach((el, index) => {
     result[index] += count * ten;
   })
 }
 
 const increase_by_num = (num, ten) => {
-  while(num) {
+  console.log(num);
+  while(num > 0) {
     result[num % 10] += ten;
-    num /= 10;
+    num = Math.floor(num / 10);
   }
 }
 
@@ -38,6 +39,8 @@ const solution = (start, end, ten) => {
 }
 
 solution(1, first, 1);
+resultString = ""
 for(let i of result) {
-  console.log(i)
+  resultString = resultString + i + " ";
 }
+console.log(resultString)
