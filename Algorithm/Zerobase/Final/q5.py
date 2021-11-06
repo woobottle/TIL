c7e66a525828cdc33d596f41edf0092f76dfd00f
woobottle <dkrnfls@gmail.com>
2021-11-07 00:03:28 +0900

    add algorithms

---
+++ Algorithm/Zerobase/Final/q5.py
@@ -0,0 +1,29 @@
+def solution(words, queries):
+  answer = []
+
+  for query in queries : 
+    count = 0
+    is_start_head = False
+    start_index = list(query).index("?")
+    replaced_query = query.replace("?", "")
+    last_index = 0
+
+    if start_index == 0 :
+      is_start_head = True
+      last_index = len(query) - 1 - start_index
+    
+    for word in words :
+      splitted_word = word[:start_index]
+      if is_start_head :
+        splitted_word = word[last_index:]
+
+      print(splitted_word, replaced_query)
+      if replaced_query == splitted_word and len(query) == len(word) :
+        count += 1
+    answer.append(count)
+  return answer
+
+
+words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
+queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
+print(solution(words, queries)) # [3, 2, 4, 1, 0]
--- Algorithm/Zerobase/Week8/q4.py
+++ Algorithm/Zerobase/Week8/q4.py
@@ -1,5 +1,19 @@
 def solution(N, K1, K2, W, V):
   answer = [[0] * (K2 + 1)] * (K1 + 1)
+  
+  for index in range(N) :
+    for w1 in range(K1, 0, -1) :
+      for w2 in range(K2, 0, -1) :
+        weight = W[index]
+        cost = V[index]
+        if w1 >= weight and w2 >= weight :
+          answer[w1][w2] = max(
+            answer[w1][w2], max(answer[w1-weight][w2] + cost , answer[w1][w2-weight] + cost))
+        elif w1 >= weight :
+          answer[w1][w2] = max(answer[w1][w2], answer[w1-weight][w2] + cost)
+        elif w2 >= weight :
+          answer[w1][w2] = max(answer[w1][w2], answer[w1][w2-weight] + cost)
+
   return answer
 
 print(solution(4, 3, 8, [1,5,6,3], [5,2,14,6])) # 25
