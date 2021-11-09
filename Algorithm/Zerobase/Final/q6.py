8cb766d08ab732ff4c13566fd70b99df9849fc76
woobottle <dkrnfls@gmail.com>
2021-11-10 00:09:47 +0900

    add algorithms

---
+++ Algorithm/Zerobase/Final/q6.py
@@ -0,0 +1,14 @@
+def solution(n, weak, dist):
+    answer = 0
+    return answer
+
+
+n = 12
+weak = [1, 5, 6, 10]
+dist = [1, 2, 3, 4]
+print(solution(n, weak, dist))
+
+n = 12
+weak = [1, 3, 4, 9, 10]
+dist = [3, 5, 7]
+print(solution(n, weak, dist))
