fe03b9419e3de077678e58b5807f678f2727226b
woobottle <dkrnfls@gmail.com>
2021-11-20 10:07:57 +0900

    add os lectures

--- Algorithm/Baekjoon/python/11053.py
+++ Algorithm/Baekjoon/python/11053.py
@@ -1,20 +1,10 @@
 n = int(input())
 l = list(map(int, input().split()))
 
-dp = [0] * n
-dp[0] = 1
-max_value = dp[0]
+dp = [1] * (n + 1)
 for i in range(1, n) :
-  if (max_value <= l[i]) :
-    dp[i] = max(dp) + 1
-    max_value = l[i]
-  else : 
-    dp[i] = max(dp)
+  for j in range(i) :
+    if l[i] > l[j] :
+      dp[i] = max(dp[i], dp[j] + 1)
 
-print(dp)
-print(dp[n-1])
-
-# 6
-# 10 20 10 30 20 50
-# 6
-# 10 20 10 20 30 40
+print(max(dp))
