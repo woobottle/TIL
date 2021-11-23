28f36e73f07018859b08b039889b83cb0bf7b7c6
woobottle <dkrnfls@gmail.com>
2021-11-24 00:09:50 +0900

    add os lectures

---
+++ Algorithm/Baekjoon/python/11279.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+heap = []
+
+for i in range(n) :
+  num = int(sys.stdin.readline())
+  if num == 0 and len(heap) == 0:
+    print(0)
+  elif num == 0 :
+    print(heapq.heappop(heap)[1])
+  else :
+    heapq.heappush(heap, (-num, num ))
