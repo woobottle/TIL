8af6fc4b0942434359fe5fccd6ac4d863bd7a18e
woobottle <dkrnfls@gmail.com>
2021-12-07 00:12:15 +0900

    add lectures

---
+++ Algorithm/Baekjoon/python/1934.py
@@ -0,0 +1,31 @@
+def test (first, second) :
+  array = [0] * (first + 1)
+  for i in range(1, first+1) :
+    if first % i == 0 :
+      array[i] = 1
+  
+  max_num = 0
+  for i in range(len(array)):
+    if array[i] == 1 and second % i == 0:
+      max_num = i
+  
+  return max_num
+
+t = int(input())
+
+for _ in range(t) :
+  a,b = map(int, input().split())
+  print((a*b) // test(a,b))
+
+
+# def gcd(a, b):
+#     if b == 0:
+#       return a
+#     return gcd(b, a % b)
+
+
+# t = int(stdin.readline())
+# while t:
+#     a, b = map(int, stdin.readline().split())
+#     print(a*b//gcd(a, b))
+#     t -= 1
