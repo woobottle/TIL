77f77e9b2e89c29bbac771b0687404a0989f5f23
woobottle <dkrnfls@gmail.com>
2021-12-09 09:10:10 +0900

    add algorithms

--- Algorithm/Baekjoon/python/1149.py
+++ Algorithm/Baekjoon/python/1149.py
@@ -1,16 +1,11 @@
 n = int(input())
 cost = []
-for _ in range(n):
+for _ in range(n) :
   cost.append(list(map(int, input().split())))
 
-for i in range(1, len(cost) - 1):
+for i in range(1, len(cost)) :
   cost[i][0] = min(cost[i-1][1], cost[i-1][2]) + cost[i][0]
   cost[i][1] = min(cost[i-1][0], cost[i-1][2]) + cost[i][1]
   cost[i][2] = min(cost[i-1][0], cost[i-1][1]) + cost[i][2]
 
-cost[n-1][0] = min(cost[n-2][1], cost[n-2][2]) + cost[n-1][0]
-cost[n-1][1] = min(cost[n-2][0], cost[n-2][2]) + cost[n-1][1]
-cost[n-1][2] = min(cost[n-2][0], cost[n-2][1]) + cost[n-1][2]
-
-print(cost)
 print(min(cost[n-1][0], cost[n-1][1], cost[n-1][2]))
