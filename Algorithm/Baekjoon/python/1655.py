2b9ec130b7ef8dfefb0db6c55d1553d8610ffeff
woobottle <dkrnfls@gmail.com>
2021-12-23 22:02:52 +0900

    add algorithms

--- Algorithm/Baekjoon/python/1655.py
+++ Algorithm/Baekjoon/python/1655.py
@@ -1,25 +1,27 @@
-# 1 1
-# 1 1 5
-# 2 1 2 5
-# 2 1 2 5 10
-# 2 -99 1 2 5 10
-# 2 -99 1 2 5 7 10
-# 5 -99 1 2 5 5 7 10
 import heapq
 import sys
 
 def BOJ1655():
   n = int(sys.stdin.readline())
-  l = heapq.heapify([])
-  num = int(sys.stdin.readline())
-  l = heapq.heappush(l, num)
-  s = heapq.heappop(l)
-  print(s)
-  heapq.heappush(l, s)
+  leftheap = []
+  rightheap = []
   
-  for i in range(n-1) :
-    heapq.heappush(l, int(sys.stdin.readline()))
+  for _ in range(n) :
+    input_num = int(sys.stdin.readline())
+
+    if len(leftheap) == len(rightheap) :
+      heapq.heappush(leftheap, (-input_num, input_num))
+    else :
+      heapq.heappush(rightheap, (input_num, input_num))
     
-    print(l[len(l) // 2])
+    if rightheap and leftheap[0][1] > rightheap[0][1] :
+      leftmin = heapq.heappop(leftheap)[1]
+      rightmin = heapq.heappop(rightheap)[0]
+      heapq.heappush(leftheap, (-rightmin, rightmin))
+      heapq.heappush(rightheap, (leftmin, leftmin))
+
+    print(leftheap[0][1])
 
 BOJ1655()
+
+# https: // art-coding3.tistory.com/44
