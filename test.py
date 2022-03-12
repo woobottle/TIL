b8244c645574a297820330e19707618ca0591a23
woobottle <dkrnfls@gmail.com>
2022-03-12 12:40:22 +0900

    add algorithms

--- test.py
+++ test.py
@@ -1,37 +1,44 @@
-# from re import A
-# import sys 
-# input = sys.stdin.readline
+def solution(n, clockwise):
+  answer = [[0 for _ in range(n)] for _ in range(n)]
+  directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+  start = [[0, 0, 0], [n-1, 0, 1], [n-1, n-1, 2],  [0, n-1, 3]]
+  index = 1
 
-# def solve(a, b) :
-#   def func(turn, a, b) :
-#     if a == 0 and b == 0 :
-#       if turn == 0 :
-#         pass
-#       if turn == 1 :
-#         pass
-
-#   dp = [[-1, -1] for _ in range(101)]
-  
-#   func(0, a, b)
-  
-
-#   print(dp[a][b])
-
+  def check_graph() :
+    for i in range(n) :
+      for j in range(n) :
+        if answer[i][j] == 0 :
+          return True
     
-# solve(0, 100) # 0, 1
-# solve(1, 2) # 1, 2
-# solve(1, 4) 
-# solve(3, 1) 
-# solve(97, 4) 
-# solve(2, 3) 
-# solve(2, 4) 
-# solve(3, 5) 
-# solve(5, 3) 
+    return False
 
-# # 내가 생각한 가장 빨리 이기는 법 -> 숫자 둘중 하나를 1로 만들어 버리자
-# # 내가 생각한 가장 느리게 지는 법 -> 1개만 빼기
+  while True :
+    flag = True
+    for i in range(len(start)) :
+      x, y, dir_index = start[i]
+      answer[x][y] = index
+      dir_x, dir_y = directions[dir_index]
+      next_x, next_y = x + dir_x, y + dir_y
+      
+      if answer[next_x][next_y] != 0 :
+        dir_index = (dir_index + 1) % 4
+        dir_x, dir_y = directions[dir_index]
+        next_x, next_y = x + dir_x, y + dir_y
 
-print(1 << 1)
-print(1 << 2)
-print(1 << 3) # 1 -> 1000
-print(1 << 4) # 1 -> 10000
+      start[i] = [next_x, next_y, dir_index]
+
+      if check_graph() == False :
+        flag = False
+        break
+    if flag == False :
+      break
+    index += 1
+
+  if clockwise :
+    for i in range(n) :
+      answer[i] = answer[i][::-1]
+  
+  return answer
+
+print(solution(5, True))
+print(solution(6, False))
