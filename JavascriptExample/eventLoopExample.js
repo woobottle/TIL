f4a05b8d37a97cac5decf2ba782f2d86a2499679
woobottle <dkrnfls@gmail.com>
2022-04-27 19:36:22 +0900

    add javascript example

--- JavascriptExample/eventLoopExample.js
+++ JavascriptExample/eventLoopExample.js
@@ -1,178 +1,216 @@
-// let a = (cb) => setTimeout(cb, 1000)
-// let b = (cb) => setTimeout(cb, 1000);
-// let c = (cb) => setTimeout(cb, 1000);
-// let d = (cb) => setTimeout(cb, 1000);
+// // let a = (cb) => setTimeout(cb, 1000)
+// // let b = (cb) => setTimeout(cb, 1000);
+// // let c = (cb) => setTimeout(cb, 1000);
+// // let d = (cb) => setTimeout(cb, 1000);
+
+// // let test = () => {
+// //   a(() => {
+// //     console.log('a');
+// //     b(() => {
+// //       console.log('b')
+// //       c(() => {
+// //         console.log('c');
+// //         d(() => {
+// //           console.log('d')
+// //         })
+// //       })
+// //     })
+// //   })
+// // };
 
 // let test = () => {
-//   a(() => {
+//   setTimeout(() => {
 //     console.log('a');
-//     b(() => {
-//       console.log('b')
-//       c(() => {
+//     setTimeout(() => {
+//       console.log('b');
+//       setTimeout(() => {
 //         console.log('c');
-//         d(() => {
-//           console.log('d')
-//         })
-//       })
-//     })
-//   })
-// };
-
-let test = () => {
-  setTimeout(() => {
-    console.log('a');
-    setTimeout(() => {
-      console.log('b');
-      setTimeout(() => {
-        console.log('c');
-        setTimeout(() => {
-          console.log('d');
-          setTimeout(() => {
-            console.log('e')
-          }, 1000)
-        }, 1000)
-      }, 1000)
-    }, 1000)
-  }, 1000)
-}
-
-test = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log('a')
-      resolve(new Promise((resolve) => {
-        setTimeout(() => {
-          console.log('b')
-          resolve(new Promise((resolve) => {
-            setTimeout(() => {
-              console.log('c');
-              resolve(new Promise((resolve) => {
-                setTimeout(() => {
-                  console.log('d')
-                  resolve(new Promise((resolve) => {
-                    setTimeout(() => {
-                      console.log('e');
-                      resolve()
-                    }, 1000)
-                  }))
-                }, 1000)
-              }))
-            }, 1000)
-          }))
-        }, 1000)
-      }));
-    }, 1000)
-  })
-}
-
-
-// test();
-
-// let a = () =>
-//   new Promise((resolve, reject) => setTimeout(() => resolve("a"), 1000));
-// let b = () =>
-//   new Promise((resolve, reject) => setTimeout(() => resolve("b"), 1000));
-// let c = () =>
-//   new Promise((resolve, reject) => setTimeout(() => reject(new Error('this is error')), 1000));
-
-// Promise.all([a(), b()]).then(data => console.log(data)) // 전부 성공해야 반환, 실패하면 실패한 것 반환
-// Promise.allSettled([a(), b(), c()]).then(data => console.log(data)); // status와 value를 같이 반환
-// Promise.any([a(), b(), c()]).then(data => console.log(data)) // 주어진 모든 프로미스 중 하나라도 이행하는 순간 반환
-// Promise.race([a(), b(), c()]).then((data) => console.log(data)); // 가장먼저 실행되는 것 반환
-
-
-// test = () => {
-//   a()
-//     .then((data) => {
-//       console.log(data);
-//       return b();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       return c();
-//     })
-//     .then((data) => {
-//       console.log(data); 
-//     })
-//     .catch(err => console.log(err))
+//         setTimeout(() => {
+//           console.log('d');
+//           setTimeout(() => {
+//             console.log('e')
+//           }, 1000)
+//         }, 1000)
+//       }, 1000)
+//     }, 1000)
+//   }, 1000)
 // }
 
-// test()
-
-
-// a()
-//   .then((data) => {
-//     console.log(data);
-//     return b();
+// test = () => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log('a')
+//       resolve(new Promise((resolve) => {
+//         setTimeout(() => {
+//           console.log('b')
+//           resolve(new Promise((resolve) => {
+//             setTimeout(() => {
+//               console.log('c');
+//               resolve(new Promise((resolve) => {
+//                 setTimeout(() => {
+//                   console.log('d')
+//                   resolve(new Promise((resolve) => {
+//                     setTimeout(() => {
+//                       console.log('e');
+//                       resolve()
+//                     }, 1000)
+//                   }))
+//                 }, 1000)
+//               }))
+//             }, 1000)
+//           }))
+//         }, 1000)
+//       }));
+//     }, 1000)
 //   })
-//   .then((data) => {
-//     console.log(data);
-//     return c();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     return d();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   });
-
-// Promise.all([a(), b(), c(), d()]).then(() => console.log('done'))
+// }
 
 
-// const makeBrowser = function() {
-//   const uiInterface = getUiInterface();
-//   const broserEngine = getBroserEngine();
-//   const renderingEngine = genRenderingEngine();
-//   const networking = getNetworking();
-//   const javascriptEngine = getJavascriptEngine();
-//   const uiBackend = getUiBackend();
-//   const dataPersistence = getDataPersistence();
-// } 
+// // test();
+
+// // let a = () =>
+// //   new Promise((resolve, reject) => setTimeout(() => resolve("a"), 1000));
+// // let b = () =>
+// //   new Promise((resolve, reject) => setTimeout(() => resolve("b"), 1000));
+// // let c = () =>
+// //   new Promise((resolve, reject) => setTimeout(() => reject(new Error('this is error')), 1000));
+
+// // Promise.all([a(), b()]).then(data => console.log(data)) // 전부 성공해야 반환, 실패하면 실패한 것 반환
+// // Promise.allSettled([a(), b(), c()]).then(data => console.log(data)); // status와 value를 같이 반환
+// // Promise.any([a(), b(), c()]).then(data => console.log(data)) // 주어진 모든 프로미스 중 하나라도 이행하는 순간 반환
+// // Promise.race([a(), b(), c()]).then((data) => console.log(data)); // 가장먼저 실행되는 것 반환
+
+
+// // test = () => {
+// //   a()
+// //     .then((data) => {
+// //       console.log(data);
+// //       return b();
+// //     })
+// //     .then((data) => {
+// //       console.log(data);
+// //       return c();
+// //     })
+// //     .then((data) => {
+// //       console.log(data); 
+// //     })
+// //     .catch(err => console.log(err))
+// // }
+
+// // test()
+
+
+// // a()
+// //   .then((data) => {
+// //     console.log(data);
+// //     return b();
+// //   })
+// //   .then((data) => {
+// //     console.log(data);
+// //     return c();
+// //   })
+// //   .then((data) => {
+// //     console.log(data);
+// //     return d();
+// //   })
+// //   .then((data) => {
+// //     console.log(data);
+// //   });
+
+// // Promise.all([a(), b(), c(), d()]).then(() => console.log('done'))
+
+
+// // const makeBrowser = function() {
+// //   const uiInterface = getUiInterface();
+// //   const broserEngine = getBroserEngine();
+// //   const renderingEngine = genRenderingEngine();
+// //   const networking = getNetworking();
+// //   const javascriptEngine = getJavascriptEngine();
+// //   const uiBackend = getUiBackend();
+// //   const dataPersistence = getDataPersistence();
+// // } 
+
+// // let a = () =>
+// //   new Promise((resolve, reject) => setTimeout(() => resolve("a"), 1000));
+// // let b = () =>
+// //   new Promise((resolve, reject) => setTimeout(() => resolve("b"), 1000));
+// // let c = () =>
+// //   new Promise((resolve, reject) => setTimeout(() => resolve('c'), 1000));
+// // let d = () =>
+// //   new Promise((resolve, reject) => setTimeout(() => resolve("d"), 1000));
+// // let e = () =>
+// //   new Promise((resolve, reject) => setTimeout(() => resolve("e"), 1000));
+
+// // const tt = (data) => 
+// //   new Promise((resolve) => setTimeout(() => resolve(data), 1000));
+
+// // let alphabets = ['a', 'b', 'c', 'd', 'e']
+
+// // alphabets.map(async alpha => console.log(await tt(alpha)));
+// // alphabets.forEach(async alpha => console.log(await tt(alpha)));
+// // for(let alpha of alphabets) {
+// //   (async function(){
+// //     console.log(await tt(alpha))
+// //   }())
+// // }
 
 // let a = () =>
 //   new Promise((resolve, reject) => setTimeout(() => resolve("a"), 1000));
 // let b = () =>
 //   new Promise((resolve, reject) => setTimeout(() => resolve("b"), 1000));
 // let c = () =>
-//   new Promise((resolve, reject) => setTimeout(() => resolve('c'), 1000));
+//   new Promise((resolve, reject) => setTimeout(() => reject(new Error('c')), 1000));
 // let d = () =>
 //   new Promise((resolve, reject) => setTimeout(() => resolve("d"), 1000));
 // let e = () =>
 //   new Promise((resolve, reject) => setTimeout(() => resolve("e"), 1000));
 
-// const tt = (data) => 
-//   new Promise((resolve) => setTimeout(() => resolve(data), 1000));
+// (async function () {
+//   try {
+//     console.log(await a());
+//     console.log(await b());
+//     console.log(await c());
+//     console.log(await d());
+//     console.log(await e());
+//   } catch(err) {
+//     console.log(err);
+//   }
+// })();
 
-// let alphabets = ['a', 'b', 'c', 'd', 'e']
+(() => {
+  console.log('normal 1')
+  requestAnimationFrame(() => {console.log('ra 1')})
+  setTimeout(() => console.log("setTimeout 1"));
+  Promise.resolve('promise 1').then(r => console.log(r))
+  console.log("normal 2");
+  setTimeout(() => console.log('setTimeout 2'))
+  console.log("normal 3");
+  Promise.resolve("promise 2").then((r) => console.log(r))
+  setTimeout(() => console.log("setTimeout 3"));
+  requestAnimationFrame(() => {
+    console.log("ra 2");
+  });
+  setTimeout(() => {
+    Promise.resolve("promise 3").then((r) => console.log(r));
+    setTimeout(() => console.log("setTimeout 3"));
+    requestAnimationFrame(() => {
+      console.log("ra 3");
+    });
+    Promise.resolve("promise 4").then((r) => console.log(r));
+    requestAnimationFrame(() => {
+      console.log("ra 4");
+    });
+    setTimeout(() => console.log("setTimeout 4"));
+  })
+})()
 
-// alphabets.map(async alpha => console.log(await tt(alpha)));
-// alphabets.forEach(async alpha => console.log(await tt(alpha)));
-// for(let alpha of alphabets) {
-//   (async function(){
-//     console.log(await tt(alpha))
-//   }())
-// }
-
-let a = () =>
-  new Promise((resolve, reject) => setTimeout(() => resolve("a"), 1000));
-let b = () =>
-  new Promise((resolve, reject) => setTimeout(() => resolve("b"), 1000));
-let c = () =>
-  new Promise((resolve, reject) => setTimeout(() => reject(new Error('c')), 1000));
-let d = () =>
-  new Promise((resolve, reject) => setTimeout(() => resolve("d"), 1000));
-let e = () =>
-  new Promise((resolve, reject) => setTimeout(() => resolve("e"), 1000));
-
-(async function () {
-  try {
-    console.log(await a());
-    console.log(await b());
-    console.log(await c());
-    console.log(await d());
-    console.log(await e());
-  } catch(err) {
-    console.log(err);
-  }
-})();
+// normal 1
+// normal 2
+// normal 3
+// promise 1
+// promise 2
+// ra 1
+// ra 2
+// setTimeout 1
+// setTimeout 2
+// setTimeout 3
