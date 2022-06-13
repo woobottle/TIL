1a6497e7be55ddecccaf3d0f236b0b55d2f69c41
woobottle <dkrnfls@gmail.com>
2022-06-13 16:30:21 +0900

    add algorithms

---
+++ Algorithm/Programmers/javascript/큰수만들기.js
@@ -0,0 +1,15 @@
+function solution(number, k) {
+  let answer = "";
+  let deleteCount = -1;
+  let answerArray = [0];
+
+  for (let i = 0; i < number.length; i++) {
+    while (deleteCount < k && answerArray[answerArray.length - 1] < number[i]) {
+      answerArray.pop();
+      deleteCount++;
+    }
+    if (answerArray.length < number.length - k) answerArray.push(number[i]);
+  }
+  answer = answerArray.join("");
+  return answer;
+}
